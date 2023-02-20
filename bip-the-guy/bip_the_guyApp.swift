b43ea959e37539874eb5def0dae7d6ec8b49d69f
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-02-19 20:39:25 -0500

    Initial Commit

---
+++ bip-the-guy/bip_the_guyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bip_the_guyApp.swift
+//  bip-the-guy
+//
+//  Created by Jonathan Wheeler Jr. on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct bip_the_guyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
