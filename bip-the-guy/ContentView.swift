585d8c57827da14caf798727852f012fcbc7fdfc
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-02-19 21:40:44 -0500

    3.3 Done

--- bip-the-guy/ContentView.swift
+++ bip-the-guy/ContentView.swift
@@ -7,15 +7,19 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var animateImage = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
+    
     var body: some View {
         VStack {
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(animateImage ? 1.0 : 0.9)
@@ -29,12 +33,23 @@ struct ContentView: View {
                 
             Spacer()
             
-            Button {
-                // todo
-            } label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
             }
-
+            .onChange(of: selectedPhoto) { newValue in
+                Task {
+                    do {
+                        if let data = try await newValue?.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data) {
+                                bipImage = Image(uiImage: uiImage)
+                            }
+                        }
+                    } catch {
+                        print("😡 ERROR: loading failed \(error.localizedDescription)")
+                    }
+                }
+            }
+            
         }
         .padding()
     }
